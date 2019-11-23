1c61d0ada2cc6e5040b37cc6341a6d843142ffe7
s70749 <dominic.engel@beuth-hochschule.de>
2019-11-23 18:05:37 +0100

    worked on LEVEL 1_1

---
+++ ALONE/Assets/Scripts/Object Animation/openDoor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour
+{
+    [SerializeField] GameObject movingDoor;
+    private AudioSource audios;
+    [SerializeField] AudioClip doorSound;
+    private bool isTriggered = false;
+
+
+    private void Start()
+    {
+        audios = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            movingDoor.GetComponent<Animator>().SetTrigger("DoorDouble_Open");
+
+        }
+
+        if (isTriggered == false)
+        {
+            audios.PlayOneShot(doorSound);
+            isTriggered = true;
+        }
+
+        if (isTriggered == false)
+        {
+            audios.Stop();
+        }
+    }
+}
