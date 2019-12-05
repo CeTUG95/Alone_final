380a2221578dde80c8c5d2741ad66603277febd1
s70749 <dominic.engel@beuth-hochschule.de>
2019-12-06 00:04:24 +0100

    added pause menu

--- ALONE/Assets/Scripts/Player/DeathUi.cs
+++ ALONE/Assets/Scripts/Player/DeathUi.cs
@@ -10,18 +10,14 @@ public class DeathUi : MonoBehaviour
 {
     [SerializeField] Canvas gameOverUi;
     [SerializeField] Canvas reticle;
-    //
     [SerializeField] Button btn;
     [SerializeField] EventSystem eventSys;
-    //
 
     private void Start()
     {
         gameOverUi.enabled = false;
-        //
         btn = GameObject.Find("PlayAgainButton").GetComponent<Button>();
         eventSys = GameObject.Find("EventSystem").GetComponent<EventSystem>();
-        //
     }
 
     public void HandleDeath()
@@ -29,11 +25,7 @@ public class DeathUi : MonoBehaviour
         gameOverUi.enabled = true;
         reticle.enabled = false;
         Time.timeScale = 0;
-        //Cursor.lockState = CursorLockMode.None;
-        //Cursor.visible = true;
-        //
         eventSys.SetSelectedGameObject(btn.gameObject);
-        //
     }
 
 }
--- ALONE/Assets/Scripts/SceneLoads/SceneLoader.cs
+++ ALONE/Assets/Scripts/SceneLoads/SceneLoader.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
-
 public class SceneLoader : MonoBehaviour
 {
 
