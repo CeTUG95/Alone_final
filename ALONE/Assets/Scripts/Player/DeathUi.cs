8a8425bc2cfa6ce5649ae5ae126a8d8ee05597a4
s70749 <dominic.engel@beuth-hochschule.de>
2019-12-10 17:48:08 +0100

    added new version of ALONE

---
+++ ALONE/Assets/Scripts/Player/DeathUi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+//
+
+public class DeathUi : MonoBehaviour
+{
+    [SerializeField] Canvas gameOverUi;
+    [SerializeField] Canvas reticle;
+    [SerializeField] Button btn;
+    [SerializeField] EventSystem eventSys;
+
+    private void Start()
+    {
+        gameOverUi.enabled = false;
+        btn = GameObject.Find("PlayAgainButton").GetComponent<Button>();
+        eventSys = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+    }
+
+    public void HandleDeath()
+    {
+        gameOverUi.enabled = true;
+        reticle.enabled = false;
+        Time.timeScale = 0;
+        eventSys.SetSelectedGameObject(btn.gameObject);
+    }
+
+}
