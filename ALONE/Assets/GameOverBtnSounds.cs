9e0874cf326d3b7fa4dd9f36dc35c235286a9759
s70749 <dominic.engel@beuth-hochschule.de>
2019-12-11 17:18:38 +0100

    ui

---
+++ ALONE/Assets/GameOverBtnSounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverBtnSounds : MonoBehaviour
+{
+
+    [SerializeField] AudioSource fx;
+    [SerializeField] AudioClip clip;
+
+    void InvokePlayAgain()
+    {
+        SceneManager.LoadScene(2);
+        Time.timeScale = 1;
+    }
+
+    void InvokeExit()
+    {
+        Application.Quit();
+    }
+
+    public void PlayAgain()
+    {
+        fx.PlayOneShot(clip);
+        SceneManager.LoadScene(2);
+        Time.timeScale = 1;
+        //Invoke("InvokePlayAgain", 2.0f);
+    }
+
+    public void Exit()
+    {
+        fx.PlayOneShot(clip);
+        //Invoke("InvokeExit", 2.0f);
+    }
+}
