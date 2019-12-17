a2dadd715ec1d2d725527e57e1f48e51c32613c9
s70749 <dominic.engel@beuth-hochschule.de>
2019-12-17 22:11:05 +0100

    cleaned up asset folder

---
+++ ALONE/Assets/Scripts/Trigger/DeathUiTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DeathUiTrigger : MonoBehaviour
+{
+    AudioSource source;
+    [SerializeField] AudioClip clip;
+
+    [SerializeField] Canvas gameOverUi;
+    [SerializeField] Canvas reticle;
+    [SerializeField] Button btn;
+    [SerializeField] EventSystem eventSys;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            source.PlayOneShot(clip);
+            gameOverUi.enabled = true;
+            reticle.enabled = false;
+            Time.timeScale = 0;
+            eventSys.SetSelectedGameObject(btn.gameObject);
+        }
+    }
+}
---
+++ ALONE/Assets/Scripts/Trigger/MusicTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTrigger : MonoBehaviour
+{
+
+    private AudioSource source;
+    [SerializeField] AudioClip clip;
+
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            source.Play();
+
+        }
+    }
+}
