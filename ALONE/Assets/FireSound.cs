5905db6477a6dc3eefc3e0e3caeb6cb0178c26a2
s70749 <dominic.engel@beuth-hochschule.de>
2020-01-19 22:46:47 +0100

    worked on Alone - end boss level

---
+++ ALONE/Assets/FireSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSound : MonoBehaviour
+{
+    private AudioSource source;
+    [SerializeField] AudioClip clip;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.Play();
+    }
+}
